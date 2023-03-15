9506f2974a5d265305c4629079981e25cca27dae
Rascal72 <123348045+Rascal72@users.noreply.github.com>
2023-03-15 15:30:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  gh repo clone matrixbotz0/PETER-PARKER-BOT
+  gh repo clone Rascal72/ARAKKAL-ABU-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PETER-PARKER-BOT
+  git clone $UPSTREAM_REPO /ARAKKAL-ABU-BOT
 fi
-cd /PETER-PARKER-BOT
+cd /ARAKKAL-ABU-BOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot..."
 python3 bot.py
